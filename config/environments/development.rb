61eed09da6328490d34ff50f96d9c74053c2a855
Aakash Jhawar <aakash.16.jhawar@gmail.com>
2019-03-22 23:01:01 +0530

    Setup email functionality for sign-up

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :test
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Show full error reports.

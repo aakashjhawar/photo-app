f1ad5e1186556b203c410af1b1df211fa93576b9
Aakash Jhawar <aakash.16.jhawar@gmail.com>
2019-03-29 21:42:41 +0530

    Removed region

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,7 +4,6 @@ if Rails.env.production?
 		:provider => 'AWS',
 		:aws_access_key_id => ENV['S3_ACCESS_KEY'],
 		:aws_secret_access_key => ENV['S3_SECRET_KEY']
-		:region                 => 'eu-west-1' # not Ireland
 		}
 		config.fog_directory = ENV['S3_BUCKET']
 	end

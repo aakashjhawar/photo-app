21818229dd25e65f068d98ca8b9deffad3378051
Aakash Jhawar <aakash.16.jhawar@gmail.com>
2019-03-29 21:55:56 +0530

    Syntax error in carrier_wave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,7 +3,7 @@ if Rails.env.production?
 		config.fog_credentials = {
 		:provider => 'AWS',
 		:aws_access_key_id => ENV['S3_ACCESS_KEY'],
-		:aws_secret_access_key => ENV['S3_SECRET_KEY']
+		:aws_secret_access_key => ENV['S3_SECRET_KEY'],
 		:region                 => ENV['REGION']
 		}
 		config.fog_directory = ENV['S3_BUCKET']

ca773d82e3fd5d253f02ec9185d8a3afdcbd968c
Aakash Jhawar <aakash.16.jhawar@gmail.com>
2019-03-29 19:26:18 +0530

    Add production image upload via AWS S3

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -5,8 +5,10 @@ class PictureUploader < CarrierWave::Uploader::Base
   process resize_to_limit: [300, 300]
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  if Rails.evn.production?
+    storage :fog
+  else
+    storage :file
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:

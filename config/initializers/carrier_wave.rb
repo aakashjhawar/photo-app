d9c7dc08f951f0a500f0d5c21c21ecec4884046c
Aakash Jhawar <aakash.16.jhawar@gmail.com>
2019-03-29 21:47:09 +0530

    Added Asia Pacific region

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,6 +4,7 @@ if Rails.env.production?
 		:provider => 'AWS',
 		:aws_access_key_id => ENV['S3_ACCESS_KEY'],
 		:aws_secret_access_key => ENV['S3_SECRET_KEY']
+		:region                 => 'ap-northeast-1' # not Ireland
 		}
 		config.fog_directory = ENV['S3_BUCKET']
 	end

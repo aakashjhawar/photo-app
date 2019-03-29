8c443cbd4e0480f4e1107cd9de14fd762acccefb
Aakash Jhawar <aakash.16.jhawar@gmail.com>
2019-03-29 21:54:04 +0530

    General region

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,7 +4,7 @@ if Rails.env.production?
 		:provider => 'AWS',
 		:aws_access_key_id => ENV['S3_ACCESS_KEY'],
 		:aws_secret_access_key => ENV['S3_SECRET_KEY']
-		:region                 => 'us-east-1' # not Ireland
+		:region                 => ENV['REGION']
 		}
 		config.fog_directory = ENV['S3_BUCKET']
 	end

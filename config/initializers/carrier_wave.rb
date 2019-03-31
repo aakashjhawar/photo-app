8d86e3a0bdfb6aa4c3578235265c30cc1af98fd0
Aakash Jhawar <aakash.16.jhawar@gmail.com>
2019-03-31 22:06:11 +0530

    replaced s3_region

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,7 +4,7 @@ if Rails.env.production?
 		:provider => 'AWS',
 		:aws_access_key_id => ENV['S3_ACCESS_KEY'],
 		:aws_secret_access_key => ENV['S3_SECRET_KEY'],
-		:s3_region                 => ENV['REGION']
+		:region                 => ENV['REGION']
 		}
 		config.fog_directory = ENV['S3_BUCKET']
 	end
